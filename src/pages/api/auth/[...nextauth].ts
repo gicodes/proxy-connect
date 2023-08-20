dd7a7d6f66d6ddad6331a06d8f43928fc6385b2e
Gideon, I <gicodes9@gmail.com>
2023-08-20 19:55:56 +0100

    ..profile

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,13 @@
 import getConfig from "next/config";
 import { ridersRepo } from "../riders/repo";
 import { randomBytes, randomUUID } from "crypto";
-import clientPromise from "../../../lib/api/mongodb";
 import NextAuth, { NextAuthOptions } from "next-auth";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const { serverRuntimeConfig } = getConfig();
 
 export const authOptions: NextAuthOptions = {
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
     CredentialsProvider({
       name: 'credentials',
@@ -45,29 +42,29 @@ export const authOptions: NextAuthOptions = {
     signIn: '/auth/sign-in',
     error: '/404',
   },
-  // session: {
-  //   // Choose how you want to save the user session.
-  //   // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
-  //   // If you use an `adapter` however, we default it to `"database"` instead.
-  //   // You can still force a JWT session by explicitly defining `"jwt"`.
-  //   // When using `"database"`, the session cookie will only contain a `sessionToken` value,
-  //   // which is used to look up the session in the database.
-  //   strategy: "database",
+  session: {
+    // Choose how you want to save the user session.
+    // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+    // If you use an `adapter` however, we default it to `"database"` instead.
+    // You can still force a JWT session by explicitly defining `"jwt"`.
+    // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+    // which is used to look up the session in the database.
+    strategy: "database",
   
-  //   // Seconds - How long until an idle session expires and is no longer valid.
-  //   maxAge: 30 * 24 * 60 * 60, // 30 days
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   
-  //   // Seconds - Throttle how frequently to write to database to extend a session.
-  //   // Use it to limit write operations. Set to 0 to always update the database.
-  //   // Note: This option is ignored if using JSON Web Tokens
-  //   updateAge: 24 * 60 * 60, // 24 hours
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60, // 24 hours
     
-  //   // The session token is usually either a random UUID or string, however if you
-  //   // need a more customized session token string, you can define your own generate function.
-  //   generateSessionToken: () => {
-  //     return randomUUID?.() ?? randomBytes(32).toString("hex")
-  //   }
-  // }
+    // The session token is usually either a random UUID or string, however if you
+    // need a more customized session token string, you can define your own generate function.
+    generateSessionToken: () => {
+      return randomUUID?.() ?? randomBytes(32).toString("hex")
+    }
+  }
 }
 
 export default NextAuth(authOptions);
